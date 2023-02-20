01a2a18f056cc50695b9b713b16236114a16de53
Oscar Santos <oscar303data@gmail.com>
2023-02-19 18:57:11 -0700

    Finished part 1 of the test successfully

--- lib/car.rb
+++ lib/car.rb
@@ -3,15 +3,21 @@ class Car
     attr_reader :make,
                 :model,
                 :monthly_payment,
-                :loan_length
+                :loan_length,
+                :color
     def initialize(brand, monthly_payment, loan_length)
         @make = brand.split.first
         @model = brand.split.last
         @monthly_payment = monthly_payment
         @loan_length = loan_length
+        @color = nil
     end
 
     def total_cost
         @monthly_payment * @loan_length
+    end
+
+    def paint!(car_color)
+        @color = car_color
     end
 end
