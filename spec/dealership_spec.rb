f4b7be397d8593a6ca525bc01e0efabeec0ce2ee
Oscar Santos <oscar303data@gmail.com>
2023-02-22 17:29:09 -0700

    it has attrboutes block pass the test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Dealership do
        
        expect(dealership).to be_a(Dealership)
     end
+
+    it 'has attributes' do
+        dealership = Dealership.new("Acme Auto", "123 Main Street")
+        
+        expect(dealership.name).to eq("Acme Auto")
+        expect(dealership.address).to eq("123 Main Street")
+     end
 end
