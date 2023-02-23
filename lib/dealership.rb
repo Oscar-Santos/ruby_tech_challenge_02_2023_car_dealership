73e1cf1c5bf737679ddd30aa44a2118bce61715b
Oscar Santos <oscar303data@gmail.com>
2023-02-22 17:40:41 -0700

    finish part two of the challenge

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -1,8 +1,18 @@
 class Dealership
     attr_reader :name,
-                :address
+                :address,
+                :inventory,
+                :inventory_count
+
     def initialize(name, address)
         @name = name
         @address = address
+        @inventory = []
+        @inventory_count = 0
+    end
+
+    def add_car(car)
+        @inventory << car
+        @inventory_count = @inventory.size
     end
 end
